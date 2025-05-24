5a4894ce46a184a99e4881fb4782311506e19492
PedroHBoro <phob.contato@gmail.com>
2025-05-24 20:26:11 -0300

    add execute function

--- TrabalhoORD.py
+++ TrabalhoORD.py
@@ -6,16 +6,43 @@ def main() -> None:
         flag: str = argv[1]
         match flag:
             case "-e":
-                pass
+                execute(data, argv[2])
             case "-p":
                 pass
             case "-c":
                 pass
-        data.close()
     except:
-        pass
-    finally:
-        pass
+        print('Erro')
+    finally: #Roda depois de tudo, mesmo se cair no except
+        if data: #Caso o arquivo ainda esteja aberto
+            print('Fechando arquivo')
+            data.close() #Fecha o arquivo
+
+def execute(dataBase, arqName: str):
+    with open(arqName, "r") as arq:
+        instructions: list = arq.read().split('\n')
+        for i in instructions:
+            [flag, *reg] = i.split()
+            strReg = "".join(reg)
+            match flag:
+                case "b":
+                    search(int(strReg), dataBase)
+                case "i":
+                    insert(strReg, dataBase)
+                case "r":
+                    remove(strReg, dataBase)
+            
+def search(regKey, dataBase):
+    print(f'Busca pelo registro de chave "{regKey}"')
+
+def insert(data, dataBase):
+    regLength = 0
+    regKey = 0
+    print(f'Inserção do registro de chave "{regKey}" ({regLength} bytes)')
+
+def remove(data, dataBase):
+    regKey = 0
+    print(f'Remoção do registro de chave "{regKey}"')
 
 if __name__ == "__main__":
-    main() 
+    main()
